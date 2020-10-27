66f6efb03e77a053b89348ff48702d9d87ae309d
Will Leyton <wleylop22@gmail.com>
2020-10-27 13:22:16 -0500

    Update Domain

--- products-app/Domain/Products.elm
+++ products-app/Domain/Products.elm
@@ -4,18 +4,7 @@ module Domain.Products exposing
     , ProductId(..)
     , ProductName(..)
     , ProductStock(..)
-    , collection
-    , createdAtToString
-    , decoder
-    , encoder
-    , idToString
-    , nameToString
-    , stockToString
     )
-
-import Json.Decode exposing (Decoder, float, list, map, string, succeed)
-import Json.Decode.Pipeline exposing (required)
-import Json.Encode as Encode
 
 
 type alias Product =
@@ -40,75 +29,3 @@ type ProductStock
 
 type ProductCreatedAt
     = ProductCreatedAt String
-
-
-idToString : ProductId -> String
-idToString (ProductId id) =
-    id
-
-
-nameToString : ProductName -> String
-nameToString (ProductName name) =
-    name
-
-
-stockToString : ProductStock -> String
-stockToString (ProductStock stock) =
-    String.fromFloat stock
-
-
-createdAtToString : ProductCreatedAt -> String
-createdAtToString (ProductCreatedAt createdAt) =
-    createdAt
-
-
-decoder : Decoder Product
-decoder =
-    succeed Product
-        |> required "product_id" idDecoder
-        |> required "product_name" nameDecoder
-        |> required "product_stock" stockDecoder
-        |> required "product_created_at" createdAtDecoder
-
-
-collection : Decoder (List Product)
-collection =
-    list decoder
-
-
-idDecoder : Decoder ProductId
-idDecoder =
-    map ProductId string
-
-
-nameDecoder : Decoder ProductName
-nameDecoder =
-    map ProductName string
-
-
-stockDecoder : Decoder ProductStock
-stockDecoder =
-    map ProductStock float
-
-
-createdAtDecoder : Decoder ProductCreatedAt
-createdAtDecoder =
-    map ProductCreatedAt string
-
-
-encoder : ( ProductName, ProductStock ) -> Encode.Value
-encoder ( name, stock ) =
-    Encode.object
-        [ ( "product_name", nameEncoder name )
-        , ( "product_stock", stockEncoder stock )
-        ]
-
-
-nameEncoder : ProductName -> Encode.Value
-nameEncoder (ProductName name) =
-    Encode.string name
-
-
-stockEncoder : ProductStock -> Encode.Value
-stockEncoder (ProductStock stock) =
-    Encode.float stock
